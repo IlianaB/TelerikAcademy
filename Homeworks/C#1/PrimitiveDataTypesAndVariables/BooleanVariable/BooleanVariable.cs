e72230f28eaa867eb43dc8cd7b70d5c23019fc8f
Iliana Bobeva <iliana.bobeva@gmail.com>
2015-01-12 19:07:03 +0200

    Primitive Data Types and Variables homework

---
+++ Homeworks/C#1/PrimitiveDataTypesAndVariables/BooleanVariable/BooleanVariable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+//Declare a Boolean variable called isFemale and assign an appropriate value corresponding to your gender.
+//Print it on the console.
+
+namespace BooleanVariable
+{
+    class BooleanVariable
+    {
+        static void Main(string[] args)
+        {
+            bool isFemale = true;
+
+            Console.WriteLine(isFemale);
+        }
+    }
+}
---
+++ Homeworks/C#1/PrimitiveDataTypesAndVariables/QuotesInStrings/QuotesInStrings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+//Declare two string variables and assign them with following value: The "use" of quotations causes difficulties.
+//Do the above in two different ways - with and without using quoted strings.
+//Print the variables to ensure that their value was correctly defined.
+
+namespace QuotesInStrings
+{
+    class QuotesInStrings
+    {
+        static void Main(string[] args)
+        {
+            string firstVar = "The \"use\" of quotations causes difficulties.";
+            string secondVar = @"The ""use"" of quotations causes difficulties.";
+
+            Console.WriteLine(firstVar);
+            Console.WriteLine(secondVar);
+        }
+    }
+}
