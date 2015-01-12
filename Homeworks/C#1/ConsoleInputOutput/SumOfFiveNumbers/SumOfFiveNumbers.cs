b0358bd849c5e63ddcb06ec00b643334469db013
Iliana Bobeva <iliana.bobeva@gmail.com>
2015-01-12 15:42:20 +0200

    Console Input/Output homework

---
+++ Homeworks/C#1/ConsoleInputOutput/SumOfFiveNumbers/SumOfFiveNumbers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+//Write a program that enters 5 numbers (given in a single line, separated by a space), calculates and prints their sum.
+
+namespace SumOfFiveNumbers
+{
+    class SumOfFiveNumbers
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Write five numbers, separated by space: ");
+            string sequence = Console.ReadLine();
+            string[] arr = sequence.Split(' ');
+            double sum = 0;
+            double numberDouble = 0;
+
+            foreach (string number in arr)
+                {
+                    numberDouble = Double.Parse(number);
+                    sum += numberDouble;
+                }
+                        
+             Console.WriteLine(sum);
+        }
+    }
+}
