84d227ebcb6b7ad7ff26e31716589b56fee51a95
Iliana Bobeva <iliana.bobeva@gmail.com>
2015-01-18 17:20:46 +0200

    Add projects

---
+++ Homeworks/C#1/ConsoleInputOutput/03. CirclePerimeterAndArea/CirclePerimeterAndArea.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+//Write a program that reads the radius r of a circle and prints its perimeter and area formatted with 2 digits after the decimal point.
+
+namespace CirclePerimeterAndArea
+{
+    class CirclePerimeterAndArea
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("The radius of the circle is: ");
+            double r = Double.Parse(Console.ReadLine());
+            double diameter = 2 * r;
+
+            Console.WriteLine("The perimeter of the circle is {0:0.00}", Math.PI * diameter);
+            Console.WriteLine("The area of the circle is {0:0.00}", Math.PI * Math.Pow(r, 2));
+        }
+    }
+}
---
+++ Homeworks/C#1/ConsoleInputOutput/09. SumOfNNumbers/SumOfNNumbers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+//Write a program that enters a number n and after that enters more n numbers and calculates and prints their sum. Note: You may need to use a for-loop.
+
+namespace SumOfNNumbers
+{
+    class SumOfNNumbers
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Write a positive integer: ");
+            int n = Int32.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write("Write a number: ");
+                int a = Int32.Parse(Console.ReadLine());
+                sum += a;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
